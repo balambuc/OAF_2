3deb83381e338cd5fc86bc9e654f33748a9bfdbe
Bence Bardosi <bardosi.bence@gmail.com>
2017-10-10 11:23:31 +0200

    Initial commit

---
+++ BigNum.cpp
@@ -0,0 +1,145 @@
+#include <cstring>
+#include <iostream>
+#include "BigNum.h"
+
+
+BigNum::BigNum(int n) {
+    while (n / 10 != 0)
+    {
+        push_first(n % 10);
+        n /= 10;
+    }
+    push_first(n);
+}
+
+BigNum::BigNum(const char* str) {
+    for (int i = 0; i < std::strlen(str); ++i)
+        push_last(str[i] - '0');
+}
+
+void BigNum::push(Node* prev, int n, Node* next) {
+    auto p = new Node(prev, n, next);
+    prev ? prev->next = p : _first = p;
+    next ? next->prev = p : _last = p;
+}
+
+BigNum& operator+(const BigNum& lhs, const BigNum& rhs) {
+    //TODO: enor nélkül megcsinálni
+
+    auto result = new BigNum();
+    BigNum lHand = lhs;
+    BigNum rHand = rhs;
+    BigNum::Enor lhsEnor = lHand.createEnor(BigNum::Enor::MODE::L2F);
+    BigNum::Enor rhsEnor = rHand.createEnor(BigNum::Enor::MODE::L2F);
+    int sum, carry = 0;
+
+    for (lhsEnor.first(), rhsEnor.first();
+         !lhsEnor.end() || !rhsEnor.end();
+         lhsEnor.next(), rhsEnor.next())
+    {
+        sum = (lhsEnor.current() ? lhsEnor.current()->value : 0)
+              + carry
+              + (rhsEnor.current() ? rhsEnor.current()->value : 0);
+        result->push_first(sum % 10);
+        carry = sum / 10;
+    }
+
+    return *result;
+}
+
+/**
+ * This is magic...basically long multiplication, buuuuuut IDK
+ * @param lhs - left side of multiplication
+ * @param rhs - right side of multiplication
+ * @return result - a reference to the result of the operation
+ */
+BigNum& operator*(const BigNum& lhs, const BigNum& rhs) {
+    auto result = new BigNum(0);
+    BigNum lHand = BigNum();
+    BigNum rHand = BigNum();
+
+    if (lhs.length() <= rhs.length())
+    {
+        lHand = lhs;
+        rHand = rhs;
+    }
+    else
+    {
+        lHand = rhs;
+        rHand = lhs;
+    }
+
+    BigNum::Enor lhsEnor = lHand.createEnor(BigNum::Enor::MODE::L2F);
+    BigNum::Enor rhsEnor = rHand.createEnor(BigNum::Enor::MODE::L2F);
+    BigNum::Node* starter = result->_last;
+    int mul, carry;
+
+    for (lhsEnor.first(); !lhsEnor.end(); lhsEnor.next())
+    {
+        carry = 0;
+        BigNum::Node* p = starter;
+        for (rhsEnor.first(); !rhsEnor.end(); rhsEnor.next())
+        {
+            mul = lhsEnor.current()->value * rhsEnor.current()->value + carry + (p ? p->value : 0);
+            carry = mul / 10;
+            mul = mul % 10;
+            if (p)
+            {
+                p->value = mul;
+                p = p->prev;
+            }
+            else result->push_first(mul);
+        }
+        if (carry) result->push_first(carry);
+        starter = starter->prev;
+    }
+
+    return *result;
+}
+
+BigNum& BigNum::operator=(const BigNum& rhs) {
+    if (&rhs == this) return *this;
+    destruct();
+    copy(rhs);
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, BigNum& bigNum) {
+    BigNum::Enor enorF2L = bigNum.createEnor();
+    for (enorF2L.first(); !enorF2L.end(); enorF2L.next())
+        os << enorF2L.current()->value;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, BigNum& bigNum) {
+    std::string num;
+    is >> num;
+    BigNum temp(num.c_str());
+    bigNum = temp;
+    return is;
+}
+
+void BigNum::destruct() {
+    Node* p;
+    while (_first != nullptr)
+    {
+        p = _first;
+        _first = p->next;
+        delete p;
+    }
+    _last = nullptr;
+}
+
+void BigNum::copy(const BigNum& other) {
+    for (Node* p = other._first; p != nullptr; p = p->next)
+        push_last(p->value);
+}
+
+int BigNum::length() const {
+    Node* p = _first;
+    int i;
+    for (i = 0; p; ++i, p = p->next);
+    return i;
+}
+
+
---
+++ BigNum.h
@@ -0,0 +1,73 @@
+#ifndef INC_2BEAD_BIGNUM_H
+#define INC_2BEAD_BIGNUM_H
+
+
+#include <ostream>
+#include <string>
+
+
+class BigNum {
+private:
+    struct Node {
+        int value;
+        Node* prev;
+        Node* next;
+
+        Node(Node* prev, int value, Node* next) : prev(prev), value(value), next(next) {}
+    };
+
+    Node* _first = nullptr;
+    Node* _last = nullptr;
+
+    void push(Node* prev, int n, Node* next);
+    void push_first(int n) { push(nullptr, n, _first); }
+    void push_last(int n) { push(_last, n, nullptr); }
+    void copy(const BigNum& other);
+    void destruct();
+
+public:
+    class Enor {
+    public:
+        enum MODE {
+            F2L, L2F
+        };
+
+    private:
+        BigNum* _bigNum;
+        Node* _ptr = nullptr;
+        Enor::MODE _m;
+
+    public:
+        Enor(BigNum* bigNum, MODE m) : _bigNum(bigNum), _m(m) {}
+
+        void first() { if (_m == L2F) _ptr = _bigNum->_last; else _ptr = _bigNum->_first; }
+
+        void next() {
+            if (_ptr == nullptr) return;
+            if (_m == L2F) _ptr = _ptr->prev; else _ptr = _ptr->next;
+        }
+
+        Node* current() { return _ptr; }
+
+        bool end() { return _ptr == nullptr; }
+    };
+
+    BigNum() = default;
+    explicit BigNum(int n);
+    explicit BigNum(const char* str);
+    BigNum(const BigNum& other) { copy(other); }
+    ~BigNum() { destruct(); }
+
+    Enor createEnor(Enor::MODE mode = Enor::MODE::F2L) { return {this, mode}; }
+    int length() const;
+
+    BigNum& operator=(const BigNum& rhs);
+
+    friend std::ostream& operator<<(std::ostream& os, BigNum& bigNum);
+    friend std::istream& operator>>(std::istream& is, BigNum& bigNum);
+    friend BigNum& operator+(const BigNum& lhs, const BigNum& rhs);
+    friend BigNum& operator*(const BigNum& lhs, const BigNum& rhs);
+};
+
+
+#endif //INC_2BEAD_BIGNUM_H
