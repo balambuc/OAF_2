1c39e11cb44284cc0b2652f0c1d1258f5e1e370b
Bence Bardosi <bardosi.bence@gmail.com>
2017-10-13 20:50:05 +0200

    Added test case for op+

--- tests/operator+.cpp
+++ tests/operator+.cpp
@@ -3,13 +3,20 @@
 #include "catch.hpp"
 #include "BigNum.h"
 
-TEST_CASE("Ket nagyszam osszeadasa", "[op+]")
+TEST_CASE("Ket nagyszam osszeadasa", "BigNum")
 {
-    std::stringstream ss;
+    std::stringstream ss[3];
 
     SECTION("0 elem vizsgalata")
     {
-        ss << BigNum(1234) + BigNum(0); //"1234"
-        CHECK(ss.str() == "1234");
+        ss[0] << BigNum(1234) + BigNum(0); //"1234"
+        CHECK(ss[0].str() == "1234");
+    }
+
+    SECTION("kommutativitas vizsgalata")
+    {
+        ss[0] << BigNum(1234) + BigNum(5678);
+        ss[1] << BigNum(5678) + BigNum(1234);
+        CHECK(ss[0].str() == ss[1].str());
     }
 }
